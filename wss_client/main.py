1b369c6cacb5e23e1767cb9fe323fc9fa9744a99
Liam <hxl1119@case.edu>
2023-03-03 18:57:27 -0500

    change example

--- wss_client/main.py
+++ wss_client/main.py
@@ -7,6 +7,7 @@ WEBSOCKET_URL = "wss://wssweb.net/ws/device/{api_key}"
 
 ws_obj = communicate.init_websocket_client(WEBSOCKET_URL.format(api_key=API_KEY))
 monitor = performance.get_performance_monitor()
+monitor.set_interval(1)  # Upload device running status data per 1s
 monitor.start()
 
 # Your other logic
