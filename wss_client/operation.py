fd4bd7ee22fb04f85f978eb576faa481774f7899
Liam <hxl1119@case.edu>
2023-03-10 19:24:40 -0500

    upload dataset

---
+++ wss_client/operation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+# @Time : 2023/3/6 14:59
+# @Author : Haozheng Li (Liam)
+# @Email : hxl1119@case.edu
+
+
+def restart():
+	pass
+
+
+def stop_motion_detect():
+	pass
