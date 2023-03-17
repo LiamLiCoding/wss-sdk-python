d83a0acf0581e153725d8a8984c14d7a57e66522
Liam <hxl1119@case.edu>
2023-03-17 19:37:44 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = []
 exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
