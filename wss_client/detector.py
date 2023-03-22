6b5572f0d579dc59ab3ba85b492894705fd1eb94
Liam <hxl1119@case.edu>
2023-03-21 22:49:42 -0400

    distance detect

--- wss_client/detector.py
+++ wss_client/detector.py
@@ -65,10 +65,12 @@ class LongDistanceIntruderDetector(BaseCameraDetector):
         self.benchmark_frame = None
         self.bg_sub = None
         self.hog = None
+        self.prev_area = 0
+        self.frame_counter = 0
         self.init_detector()
     
     def init_detector(self):
-        self.bg_sub = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=10, detectShadows=False)
+        self.bg_sub = cv2.createBackgroundSubtractorMOG2(history=700, varThreshold=30, detectShadows=False)
         self.hog = cv2.HOGDescriptor()
         self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
@@ -76,8 +78,9 @@ class LongDistanceIntruderDetector(BaseCameraDetector):
         self.event = event.get_event_controller().create_event('intruder', dict)
 
     def detect(self, frame):
+        self.frame_counter += 1
         frame_copy = frame.copy()
-        frame_copy = cv2.blur(frame_copy, (5, 5))
+        frame_copy = cv2.GaussianBlur(frame_copy, (11, 11), 0)
         foreground_mask = self.bg_sub.apply(frame_copy)
 
         # Apply a threshold to the foreground mask to create a binary image
@@ -86,14 +89,28 @@ class LongDistanceIntruderDetector(BaseCameraDetector):
         cleaned = cv2.GaussianBlur(thresh, (9, 9), 0)
         contours, hierarchy = cv2.findContours(cleaned, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        for contour in contours:
-            if cv2.contourArea(contour) < 1200:
-                continue
+        max_contour = max(contours, key=cv2.contourArea)
 
-            x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2, lineType=cv2.LINE_AA)
-            mid_point = (int(x + w / 2.0), int(y + h / 2.0))
-            cv2.circle(frame, mid_point, 3, (255, 0, 255), 6)
+        if cv2.contourArea(max_contour) < 1200:
+            return frame
+
+        x, y, w, h = cv2.boundingRect(max_contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2, lineType=cv2.LINE_AA)
+        mid_point = (int(x + w / 2.0), int(y + h / 2.0))
+        cv2.circle(frame, mid_point, 3, (255, 0, 255), 6)
+
+        area = w * h
+        if self.prev_area and not self.frame_counter % 25:
+
+            print(self.frame_counter, area, self.prev_area)
+            print(int((area - self.prev_area) / self.prev_area * 100))
+        # if area > self.prev_area:
+        #     approaching = True
+        # else:
+        #     approaching = False
+
+        if not self.frame_counter % 26:
+            self.prev_area = area
 
         if self.event:
             self.event.set_value({'event_id': self.INTRUDER_EVENT1, 'data': None})
